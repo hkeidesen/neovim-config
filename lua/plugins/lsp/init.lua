28f12b26bf1fd5a992d04bd932a45badaae60c55
Hans-Kristian N. Eidesen <hans-kristian@moenmarin.no>
2024-03-14 20:44:07 +0100

    21

---
+++ lua/plugins/lsp/init.lua
@@ -0,0 +1,48 @@
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+		"folke/neoconf.nvim",
+		'folke/neodev.nvim',
+		'hrsh7th/cmp-nvim-lsp',
+		"pmizio/typescript-tools.nvim",
+	},
+	config = function()
+		require("neoconf").setup({})
+		require("neodev").setup({})
+		require("mason").setup()
+		local server_settings = require("lsp.servers")
+
+		require('mason-lspconfig').setup({
+			ensure_installed = server_settings.default_servers
+		})
+		local lspconfig = require('lspconfig')
+
+		server_settings.setup()
+
+		print("halla")
+		require('mason-lspconfig').setup_handlers({
+			function(server_name)
+				print("server_name", server_name)
+				if server_settings.is_disabled(server_name) then
+					return
+				end
+				lspconfig[server_name].setup(
+					server_settings.get_config(server_name)
+				)
+			end,
+		})
+
+		-- since we're using typescript-tools.nvim instead of the plain tsserver
+		-- it can't be installed by mason and thus needs special handling
+		if not server_settings.is_disabled("tsserver") then
+			require("typescript-tools").setup(
+				server_settings.get_config("tsserver")
+			)
+		end
+
+		-- setup diagnostics
+		require("plugins.lsp.diagnostics").setup()
+	end,
+}
